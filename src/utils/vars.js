3e798a91209adacad8e0ffe1b9dfbbb4eb5ba3d2
Jonas <jonas.groendahlxd@gmail.com>
2019-06-05 13:42:55 +0200

    new

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,34 +1,35 @@
-import sc1 from "../assets/Screenshot from 2019-05-20 10-30-07.png";
-import sc2 from "../assets/Screenshot from 2019-05-20 10-18-00.png";
-import sc3 from "../assets/Screenshot from 2019-05-20 10-18-05.png";
-import sc4 from "../assets/Screenshot from 2019-05-20 10-18-24.png";
-import sc5 from "../assets/Screenshot from 2019-05-20 10-18-31.png";
-import sc6 from "../assets/Screenshot from 2019-05-20 10-18-36.png";
-import sc7 from "../assets/Screenshot from 2019-05-20 10-18-47.png";
-import sc8 from "../assets/Screenshot from 2019-05-20 10-24-43.png";
-import sc9 from "../assets/Screenshot from 2019-05-20 10-32-31.png";
-import sc10 from "../assets/Screenshot from 2019-05-20 10-33-45.png";
-import sc11 from "../assets/Screenshot from 2019-05-20 10-38-43.png";
-import sc12 from "../assets/Screenshot from 2019-05-20 10-39-31.png";
-import sc13 from "../assets/Screenshot from 2019-05-21 08-48-50.png";
-import ss_dialog_override from "../assets/Screenshot from 2019-05-21 15-17-51.png";
-
 import ex1 from "../assets/home_extra_1.png";
 import ex2 from "../assets/home_extra_2.png";
 import ex3 from "../assets/class_detail_extra_1.jpg";
 
 import slide_home from "../assets/slides/Slide1.PNG";
 import slide_home_expanded from "../assets/slides/Slide2.PNG";
-import slide_schedule from "../assets/slides/Slide3.PNG";
-import slide_classdetail from "../assets/slides/Slide4.PNG";
-import slide_classdetail_playing from "../assets/slides/Slide5.PNG";
-import slide_classdetail_scrolled from "../assets/slides/Slide6.PNG";
-import slide_cd_dialog from "../assets/slides/Slide7.PNG";
-import slide_language from "../assets/slides/Slide8.PNG";
-import slide_search from "../assets/slides/Slide9.PNG";
-import slide_search_tab_2 from "../assets/slides/Slide10.PNG";
-import slide_admin_tab_3 from "../assets/slides/Slide11.PNG";
-import slide_admin_tab_2 from "../assets/slides/Slide12.PNG";
+import slide_not_found from "../assets/slides/Slide3.PNG";
+import slide_schedule from "../assets/slides/Slide4.PNG";
+import slide_classdetail from "../assets/slides/Slide5.PNG";
+import slide_classdetail_playing from "../assets/slides/Slide6.PNG";
+import slide_classdetail_dialog from "../assets/slides/Slide7.PNG";
+import slide_classdetail_dialog_disable_od from "../assets/slides/Slide8.PNG";
+import slide_classdetail_passcode from "../assets/slides/Slide9.PNG";
+import slide_classdetail_dialog_scheduled from "../assets/slides/Slide10.PNG";
+import slide_lang from "../assets/slides/Slide11.PNG";
+import s_search_tab1 from "../assets/slides/Slide12.PNG";
+import s_search_tab2 from "../assets/slides/Slide13.PNG";
+import a_tab3 from "../assets/slides/Slide14.PNG";
+import a_tab1 from "../assets/slides/Slide15.PNG";
+import not_found from "../assets/slides/Slide16.PNG";
+import b_start from "../assets/slides/Slide17.PNG";
+import b_start_2 from "../assets/slides/Slide18.PNG";
+import b_start_3 from "../assets/slides/Slide19.PNG";
+import b_browse from "../assets/slides/Slide20.PNG";
+import b_setup_1 from "../assets/slides/Slide22.PNG";
+import b_setup_2 from "../assets/slides/Slide23.PNG";
+import b_setup_3 from "../assets/slides/Slide24.PNG";
+import b_setup_4 from "../assets/slides/Slide25.PNG";
+import b_od_app from "../assets/slides/Slide26.PNG";
+import b_od_app_dialog from "../assets/slides/Slide27.PNG";
+import b_od_preparing from "../assets/slides/Slide28.PNG";
+import b_done from "../assets/slides/Slide29.PNG";
 
 export const URL = process.env.NODE_ENV === "development" ? "http://localhost:3001/v3/translation" : "https://api-wexer.herokuapp.com/v3/translation";
 
@@ -52,6 +53,15 @@ const steps = [
     extras: [ex1, ex2]
   },
   {
+    text: "Class not found page",
+    image: [not_found],
+    fields: {
+      errorBtn: "Back to home screen",
+      errorText: "The class you attempted to view is currently not downloaded onto the player yet",
+      errorText2: "Try again later"
+    }
+  },
+  {
     text: "Schedule",
     image: [slide_schedule],
     fields: {
@@ -65,7 +75,7 @@ const steps = [
   },
   {
     text: "Class detail page",
-    image: [slide_classdetail, slide_classdetail_playing, slide_classdetail_scrolled],
+    image: [slide_classdetail, slide_classdetail_playing],
     fields: {
       playBtn: "Play",
       qr: "Do this class at home: scan code & save for later",
@@ -84,7 +94,7 @@ const steps = [
   },
   {
     text: "Class detail page - dialog",
-    image: [slide_cd_dialog, ss_dialog_override],
+    image: [slide_classdetail_dialog, slide_classdetail_dialog_disable_od],
     fields: {
       title: "Class playing",
       prompt: "Are you sure that you would like to cancel ongoing class?",
@@ -97,7 +107,7 @@ const steps = [
   },
   {
     text: "Class detail page - dialog passcode",
-    image: [sc10],
+    image: [slide_classdetail_passcode],
     fields: {
       passcode: "Enter passcode",
       delete: "Delete"
@@ -105,14 +115,21 @@ const steps = [
   },
   {
     text: "Class detail page - dialog upcoming class",
-    image: [sc11],
+    image: [slide_classdetail_dialog_scheduled],
     fields: {
       scheduledClassMessage: "Selection conflicts with a scheduled class. Your class will play, but will be interrupted by the scheduled class"
     }
   },
   {
+    text: "Home screen - language dialog",
+    image: [slide_lang],
+    fields: {
+      title: "Choose your language"
+    }
+  },
+  {
     text: "Search page - main tab",
-    image: [sc2],
+    image: [s_search_tab1],
     fields: {
       btn1: "Category filter",
       btn2: "Search",
@@ -141,7 +158,7 @@ const steps = [
   },
   {
     text: "Search page - search tab",
-    image: [sc3],
+    image: [s_search_tab2],
     fields: {
       searchPlaceholder: "Tap to search by name, category or tags",
       keyboardSpace: "Space",
@@ -149,16 +166,12 @@ const steps = [
     }
   },
   {
-    text: "Home screen - language dialog",
-    image: [slide_language],
+    text: "Admin section",
+    image: [a_tab3, a_tab1],
     fields: {
-      title: "Choose your language"
-    }
-  },
-  {
-    text: "Admin",
-    image: [slide_admin_tab_2],
-    fields: {
+      ic: "Launch",
+      moto1: "Show tiles",
+      moto2: "Hide tiles",
       title: "Admin",
       tab1: "General",
       tab2: "Advanced",
@@ -168,24 +181,6 @@ const steps = [
       opt1: "Player interface",
       opt2: "Type",
       opt3: "Volume"
-    }
-  },
-  {
-    text: "Admin - Add on tab",
-    image: [slide_admin_tab_3],
-    fields: {
-      ic: "Launch",
-      moto1: "Show tiles",
-      moto2: "Hide tiles"
-    }
-  },
-  {
-    text: "Class not found page",
-    image: [sc12],
-    fields: {
-      errorBtn: "Back to home screen",
-      errorText: "The class you attempted to view is currently not downloaded onto the player yet",
-      errorText2: "Try again later"
     }
   },
   /*
@@ -273,20 +268,26 @@ const steps = [
     }
   },*/
   {
-    text: "Extras",
-    image: ["https://pngimage.net/wp-content/uploads/2018/05/extra-extra-png-1.png"],
+    text: "Bodybike start screen",
+    image: [b_start, b_start_2, b_start_3],
     fields: {
-      relatedByCategory: "Related content by category",
-      relatedByProvider: "Related content by provider",
-      description1: "Choose BIKE mode for hundreds of cycle workouts. BODY BIKE will show your live data on-screen.",
-      description2: "Choose FLOOR MODE for on floor exercises. BODY BIKE will not show any data on screen when not in BIKE MODE.",
       title: "Welcome to Body Bike. Select your training mode to continue. ",
       btn: "Go",
-      title_ride: "Ride complete", // TODO when applying mapping in App
-      description: "See your ride summary below. Scan the QR code on your phone to save your workout and sync with Strava.",
-      cooldownTitle: "Cool down routines",
-      cooldownDescription: "Switch the BODY BIKE 360 into Floor mode and try a cool down routine.",
-      bikemode: "Just ride",
+      description1: "Choose BIKE mode for hundreds of cycle workouts. BODY BIKE will show your live data on-screen.",
+      description2: "Choose FLOOR MODE for on floor exercises. BODY BIKE will not show any data on screen when not in BIKE MODE."
+    }
+  },
+  {
+    text: "Bodybike main",
+    image: [b_browse],
+    fields: {
+      bikemode: "Just ride"
+    }
+  },
+  {
+    text: "Bodybike setup",
+    image: [b_setup_1, b_setup_2, b_setup_3, b_setup_4],
+    fields: {
       title_setupbodybike: "Setup your ride",
       description_setupbodybike: "Input your data so BODY BIKE can calculate your calorie burn",
       select1: "Gender",
@@ -299,6 +300,35 @@ const steps = [
       btn2: "Skip",
       btn3: "Back to main menu",
       next: "Next"
+    }
+  },
+  {
+    text: "Bodybike completed class",
+    image: [b_done], // missing 1 sc
+    fields: {
+      title: "Ride complete", // TODO when applying mapping in App
+      description: "See your ride summary below. Scan the QR code on your phone to save your workout and sync with Strava.",
+      cooldownTitle: "Cool down routines",
+      cooldownDescription: "Switch the BODY BIKE 360 into Floor mode and try a cool down routine."
+    }
+  },
+  {
+    text: "Extras",
+    image: ["https://pngimage.net/wp-content/uploads/2018/05/extra-extra-png-1.png", b_od_app, b_od_app_dialog, b_od_preparing],
+    fields: {
+      relatedByCategory: "Related content by category",
+      relatedByProvider: "Related content by provider",
+      output: "Output",
+      summary: "Summary",
+      power: "Power (w)",
+      cadence: "Cadence (rpm)",
+      speed: "Speed (kmh)",
+      calories: "Calories",
+      distance: "Distance (km)",
+      time: "Elapsed time",
+      btn1: "End ride",
+      continue: "Continue",
+      splash: "Preparing your ride summary"
     }
   }
 ];
